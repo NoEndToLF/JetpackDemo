60be234ed71488f6e9dee884259541ca1aab8a33
wangxiaoyun <wangxiaoyun@zhufaner.com>
2019-11-25 18:24:10 +0800

    写博客

--- app/src/main/java/com/wxy/jetpackdemo/lifecycle/LifecycleActivity.java
+++ app/src/main/java/com/wxy/jetpackdemo/lifecycle/LifecycleActivity.java
@@ -1,6 +1,8 @@
 package com.wxy.jetpackdemo.lifecycle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.ReportFragment;
 
 import android.os.Bundle;
 
